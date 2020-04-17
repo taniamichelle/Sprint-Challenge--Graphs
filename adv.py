1da5744a000ad69a1a598b8a880302732a394aae
tania <tanislavens@yahoo.com>
2020-04-17 15:01:47 -0400

    removed stack class as it was not used

--- adv.py
+++ adv.py
@@ -23,19 +23,6 @@ class Queue():
     def size(self):
         return len(self.queue)
 
-class Stack():
-    def __init__(self):
-        self.stack = []
-    def push(self, value):
-        self.stack.append(value)
-    def pop(self):
-        if self.size() > 0:
-            return self.stack.pop()
-        else:
-            return None
-    def size(self):
-        return len(self.stack)
-
 # Map Options:
 # You may uncomment the smaller graphs for dev and testing purposes.
 # map_file = "maps/test_line.txt"
